44d493695049773265ed3893ddb72f2b34d5eecb
Oleksandr Sokolov <osoko@t7v3wgg95c.lan>
2023-02-15 18:08:36 +0200

    workspace is created

--- rest/workspaces.go
+++ rest/workspaces.go
@@ -1,7 +1,7 @@
 package rest
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -54,24 +54,19 @@ func WorkspacesListHandler() http.HandlerFunc {
 	})
 }
 
+type WorkspaceCreateInputData struct {
+	Name string
+}
+
 func WorkspacesCreate() http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		wss := GetWorkspaceAccess(r.Context())
-		lst := []workspace.WorkspaceGetRequest{}
-		usr := user.GetUser(r.Context())
-		for _, ws := range wss {
-			wsgr := workspace.WorkspaceGetRequest{Name: ws.Name, Description: ws.Description}
-			roles := []string{}
-			if ws.UserHasAccess(usr) {
-				roles = append(roles, "user")
-			}
-			if ws.UserHasAdminAccess(usr) {
-				roles = append(roles, "admin")
-			}
-			wsgr.Roles = roles
-			lst = append(lst, wsgr)
+		var p WorkspaceCreateInputData
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 
 		userHomeDir, err := os.UserHomeDir()
@@ -94,21 +89,70 @@ func WorkspacesCreate() http.HandlerFunc {
 			WriteResponse(w, http.StatusInternalServerError, nil, nil, "workspace")
 		}
 
-		nsName := &corev1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "my-new-namespace",
+		//nsName := &corev1.Namespace{
+		//	ObjectMeta: metav1.ObjectMeta{
+		//		Name: "my-new-namespace1",
+		//	},
+		//}
+		//
+		//namespace, err := clientset.CoreV1().Namespaces().Create(context.Background(), nsName, metav1.CreateOptions{})
+		//if err != nil {
+		//	WriteResponse(w, http.StatusInternalServerError, nil, struct {
+		//		Error error
+		//	}{Error: err}, "workspace")
+		//}
+
+		//apiVersion: v1
+		//kind: ConfigMap
+		//metadata:
+		//labels:
+		//	app.kubernetes.io/component: "workspace-config"
+		//	app.kubernetes.io/part-of: "flowify"
+		//name: "sandbox-project-a"
+		//namespace: "argo"
+		//data:
+		//roles: "[[\"sandbox-developer\"]]"
+
+		cm := corev1.ConfigMap{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ConfigMap",
+				APIVersion: "v1",
 			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "workspace-test",
+				Namespace: "argo",
+				Labels:    map[string]string{"app.kubernetes.io/component": "workspace-config", "app.kubernetes.io/part-of": "flowify"},
+			},
+			Data: map[string]string{"roles": "[\"sandbox-developer\"]", "projectName": "example-workspace"},
 		}
 
-		namespace, err := clientset.CoreV1().Namespaces().Create(context.Background(), nsName, metav1.CreateOptions{})
+		CMOpt := metav1.CreateOptions{}
+
+		_, err = clientset.CoreV1().ConfigMaps("argo").Create(r.Context(), &cm, CMOpt)
 		if err != nil {
 			WriteResponse(w, http.StatusInternalServerError, nil, struct {
-				Error error
-			}{Error: err}, "workspace")
+				Error string
+			}{Error: fmt.Sprintf("error getting user home dir: %v\n", err)}, "workspace")
 		}
+
+		//configMaps, err := clientset.CoreV1().ConfigMaps("argo").List(r.Context(), metav1.ListOptions{})
+		//if err != nil {
+		//	WriteResponse(w, http.StatusInternalServerError, nil, struct {
+		//		Error string
+		//	}{Error: fmt.Sprintf("error getting user home dir: %v\n", err)}, "workspace")
+		//}
+		//var cmlist []string
+		//for _, cm := range configMaps.Items {
+		//	str, _ := json.Marshal(cm)
+		//	cmlist = append(cmlist, fmt.Sprintf("CM %s", str))
+		//}
 
 		WriteResponse(w, http.StatusOK, nil, struct {
 			NamespaceName string
-		}{NamespaceName: namespace.Name}, "workspace")
+			CM            []string
+		}{
+			//NamespaceName: namespace.Name,
+			//CM: cmlist,
+		}, "workspace")
 	})
 }
